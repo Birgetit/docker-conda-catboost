7c2ee0a21e93d48bd1a91e197652cc9a8f69375d
Birgetit <fox-4@yandex.ru>
2018-01-28 22:50:24 +0300

    added pyodbc
    build-essential, unixodbc-dev, pyodbc

--- meow-deploy/build.sh
+++ meow-deploy/build.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 
 echo "Installing python, wget and openssh-server..."
-sudo apt-get -y install python3 python3-setuptools wget bzip2
+sudo apt-get -y install python3 python3-setuptools wget bzip2 build-essential unixodbc-dev
 
 echo "UseDNS no" | sudo tee -a /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" | sudo tee -a /etc/ssh/sshd_config
@@ -16,4 +16,4 @@ bash ./Anaconda3-4.2.0-Linux-x86_64.sh -b -p /meow-deploy/conda
 export PATH="/meow-deploy/conda/bin:$PATH"
 
 cd /meow-deploy/conda/bin
-sudo /meow-deploy/conda/bin/pip install catboost
+sudo /meow-deploy/conda/bin/pip install catboost pyodbc
